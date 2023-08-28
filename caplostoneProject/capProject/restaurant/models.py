692716db2326e28458a843a5e364722ebb86989f
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-08-28 13:16:28 +0300

    DRF

--- caplostoneProject/capProject/restaurant/models.py
+++ caplostoneProject/capProject/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):    
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+class MenuItem(models.Model):
+    Title = models.CharField(max_length=5)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
--- caplostoneProject/capProject/restaurant/views.py
+++ caplostoneProject/capProject/restaurant/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import generics
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset =  MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# class bookingView(APIView):
+#     def get(self, request):
+#         item = Booking.objects.all()
+#         serializer = bookingSerializer(item, many=True)
+#         return Response(serializer.data)
+    
+
+# class menuView(APIView):
+#     def post(self, request):
+#         serializer = menuSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "success", "data": serializer.data})
